1b2dd29d0f7a69a1dce94f53460824d020467023
Aleksandra <lilit.valek@mail.ru>
2023-12-19 20:03:28 +0300

    Добавьте файлы проекта.

---
+++ Catmint/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Catmint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
